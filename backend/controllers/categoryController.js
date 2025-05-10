40c8d89b1efb7b4d337a65ede8dbf2f87dea4686
muh-habeeb <muhammedhabeebrahmam@gmail.com>
2025-05-11 01:32:08 +0530

    category contolle compleeted not checkd

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,3 +1,4 @@
+import { request } from "express";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import Category from "../models/categoryModel.js";
 import genToken from "../utils/userToken.js";
@@ -5,5 +6,25 @@ import mongoose from "mongoose";
 
 const createCategory = asyncHandler(async (req, res) => {
   try {
-  } catch (error) {}
+    const { name } = req.body;
+    if (!name) {
+      return res
+        .status(403)
+        .json({ request: "success",message:"invalid data provided",error:'Name is required', MESSAGE: "INVALID_DATA" });
+    }
+
+    const existingCategory = await Category.find({ name });
+    if (existingCategory) {
+      return res
+        .status(403)
+        .json({ request: "success",message:"category already exist", MESSAGE: "ALREADY_EXIST" });
+    }
+    const  category=await new Category({name}).save()
+    res.status(201).json({ request: "success",message:'new category added', MESSAGE: "PRODUCT_ADDED",data:category });
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+  }
 });
+
+export { createCategory };
--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 
 import {authenticated,authorizedAdmin} from '../middlewares/authMiddleWare.js'
+import { createCategory } from "../controllers/categoryController.js";
 
 router.route("/").post(createCategory);
 
