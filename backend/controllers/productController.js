12798aaaaf82b80628ca3d964ba6ed8ce9c1eb68
muh-habeeb <muhammedhabeebrahmam@gmail.com>
2025-05-16 23:46:27 +0530

    review added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -134,7 +134,7 @@ const updateProductDetails = asyncHandler(async (req, res) => {
       data: product,
     });
   } catch (error) {
-    return res.status(400).json({
+    return res.status(500).json({
       request: "success",
       message: error.message,
     });
@@ -160,4 +160,149 @@ const removeProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { addProduct, updateProductDetails, removeProduct };
+const fetchProduct = asyncHandler(async (req, res) => {
+  try {
+    const pageSize = 6;
+    const keyword = req.query.keyword
+      ? { name: { $regex: req.query.keyword, $options: "i" } }
+      : {};
+    const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword }).limit(pageSize);
+
+    return res.status(200).json({
+      code: 200,
+      request: "success",
+      message: `the products `,
+      MESSAGE: "PRODUCTS",
+      length: products.length,
+      data: products,
+      page: 1,
+      pages: Math.ceil(count.pageSize),
+      hasMore: false,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      request: "success",
+      message: error.message,
+      MESSAGE: "CAN'T_GET_PRODUCTS",
+    });
+  }
+});
+
+//fetch by id
+const fetchProductById = asyncHandler(async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      return res.status(200).json({
+        code: 200,
+        request: "success",
+        message: "the product",
+        MESSAGE: "THE_PRODUCT",
+        data: product,
+      });
+    } else {
+      return res.status(404).json({
+        request: "success",
+        message: "Product not found",
+        MESSAGE: "PRODUCT_NOT_FOUND",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      request: "success",
+      message: error.message,
+      MESSAGE: "internal server error",
+    });
+  }
+});
+
+// /fetch all product
+const fetchAllProduct = asyncHandler(async (req, res) => {
+  try {
+    const products = await Product.find({})
+      .populate("category")
+      .limit(12)
+      .sort({ createdAt: -1 });
+    res.status(200).json({
+      code: 200,
+      request: "success",
+      message: "all products",
+      MESSAGE: "ALL_PRODUCT",
+      length: products.length,
+      data: products,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      request: "success",
+      message: error.message,
+      MESSAGE: "internal server error",
+    });
+  }
+});
+
+const addProductReview = asyncHandler(async (req, res) => {
+  try {
+    const { rating, comment } = req.body;
+    console.log('req',req.user);
+    
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      const alreadyReviewed = product.reviews.find(
+        (r) => r.user.toString() === req.user._id.toString()
+      );
+
+      if (alreadyReviewed) {
+        res.status(200).json({
+          code: 400,
+          request: "success",
+          message: "product already reviewed",
+          MESSAGE: "ALREADY_REVIEWED",
+          data: [],
+        });
+      }
+      const review = {
+        name: req.user.username,
+        rating: Number(rating),
+        comment,
+        user: req.user._id,
+      };
+      product.reviews.push(review);
+      product.numReviews = product.reviews.length;
+      product.rating =
+        product.reviews.reduce((acc, item) => item.rating * acc, 0) /
+        product.reviews.length;
+      let p = await product.save();
+      res.status(201).json({
+        code: 201,
+        request: "success",
+        message: "review added",
+        MESSAGE: "REVIEW_ADDED",
+        data: p,
+      });
+    } else {
+      return res.status(404).json({
+        request: "success",
+        message: "product not found",
+        MESSAGE: "PRODUCT_NOT_FOUND",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      request: "success",
+      message: error.message,
+      MESSAGE: "internal server error",
+    });
+  }
+});
+
+export {
+  addProduct,
+  updateProductDetails,
+  fetchProduct,
+  removeProduct,
+  fetchProductById,
+  fetchAllProduct,
+  addProductReview,
+};
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -10,15 +10,27 @@ import {
   addProduct,
   updateProductDetails,
   removeProduct,
+  fetchProduct,
+  fetchProductById,
+  fetchAllProduct,
+  addProductReview,
 } from "../controllers/productController.js";
 
 import checkId from "../middlewares/checkId.js";
 
 router
   .route("/")
+  .get(fetchProduct)
   .post(authenticated, authorizedAdmin, formidable(), addProduct); //what  is formidable
+router.route("/allproducts").get(fetchAllProduct);
+
+router
+  .route("/:id/reviews")
+  .post(authenticated, authorizedAdmin, addProductReview);
+
 router
   .route("/:id")
+  .get(fetchProductById)
   .put(authenticated, authorizedAdmin, formidable(), updateProductDetails)
   .delete(authenticated, authorizedAdmin, removeProduct);
 export default router;
