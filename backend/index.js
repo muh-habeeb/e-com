7b2ef17f4469a80b232812d3b0928f60fc5d6abe
muh-habeeb <muhammadhabeebrahmam@gmail.com>
2025-09-07 22:10:30 +0530

    index.js modified for deploy

--- backend/index.js
+++ backend/index.js
@@ -40,3 +40,9 @@ app.use("/api/payment", paymentRoutes);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 app.listen(port, () => console.log(`Server running on PORT ${port}`));
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
