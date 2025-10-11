a952829204ca1a876aedd0f02acfa1e9617ad80e
shayan <shayanmohammed950@gmail.com>
2025-10-11 09:20:18 +0530

    e com

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -18,7 +18,9 @@ const createRazorpayOrder = asyncHandler(async (req, res) => {
   if (!amount) {
     return res.status(400).json({ message: "Amount is required" });
   }
-  amount = Number(amount) * 100;
+  console.log(amount);
+  amount = Math.round(Number(amount) * 100);
+console.log(amount);
 
   const options = {
     amount,
